2683a81a27ce4a46c21748c2faa67a0f0ccfebca
mzsoba <owt4832@gmail.com>
2025-02-23 02:01:25 +0900

    feat/ 로그인 페이지 및 회원 가입 페이지 병합

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,95 @@
-import React from "react";
+import AuthForm from "../component/AuthForm";
+import { useNavigate } from "react-router-dom";
+import Api from "../api/auth";
+import { useUserStore } from "../zustand/mbtiStore";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const navigate = useNavigate();
+  const { isLoginMode, setIsLoginMode } = useUserStore((state) => state);
+  const { formState, onChangeHandler, resetForm } = AuthForm({
+    id: "",
+    password: "",
+    nickname: "",
+  });
+  const { id, password, nickname } = formState;
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      if (isLoginMode) {
+        const { data } = await Api.Api.post("/login", {
+          id: formState.id,
+          password: formState.password,
+        });
+
+        console.log("Login response =>", data);
+
+        alert("로그인이 완료되었습니다.");
+
+        localStorage.setItem("accessToken", data.accessToken);
+        localStorage.setItem("user", JSON.stringify(data));
+
+        navigate("/");
+      } else {
+        await Api.Api.post("/register", {
+          id: formState.id,
+          password: formState.password,
+          nickname: formState.nickname,
+        });
+
+        alert("회원가입이 완료되었습니다.");
+
+        // 로그인 모드로 전환
+        setIsLoginMode(true);
+
+        resetForm();
+      }
+    } catch (error) {
+      console.error(error);
+      alert("에러가 발생했습니다. 다시 시도해 주세요.");
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>{isLoginMode ? "로그인" : "회원가입"}</h1>
+        <form onSubmit={onSubmitHandler}>
+          <input
+            name="id"
+            placeholder="아이디 (4~10글자)"
+            minLength={4}
+            maxLength={10}
+            value={id}
+            onChange={onChangeHandler}
+          ></input>
+          <input
+            name="password"
+            placeholder="비밀번호 (4~15글자)"
+            minLength={4}
+            maxLength={15}
+            value={password}
+            onChange={onChangeHandler}
+          ></input>
+          {!isLoginMode && (
+            <input
+              name="nickname"
+              placeholder="닉네임 (최대 8자)"
+              minLength={1}
+              maxLength={8}
+              value={nickname}
+              onChange={onChangeHandler}
+            ></input>
+          )}
+          <button>{isLoginMode ? "로그인" : "회원가입"}</button>
+          <br />
+          <span onClick={() => setIsLoginMode(!isLoginMode)}>
+            {!isLoginMode ? "로그인으로" : "회원가입으로"}
+          </span>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default SignUpPage;
