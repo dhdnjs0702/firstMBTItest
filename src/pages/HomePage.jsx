27ae4028468707a69be3f23f6629cdc90a18aef9
mzsoba <owt4832@gmail.com>
2025-02-23 02:00:37 +0900

    feat/ 로그인 상태별 처리 로직 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import CompNavBar from "../component/CompNavBar";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const navigate = useNavigate();
+
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    setIsLogin(!!token); 
+    console.log("Token in useEffect:", token);
+  }, []); 
+
+  useEffect(() => {
+    if (isLogin) {
+      console.log("User is logged in, navigating to /test");
+      navigate("/test"); 
+    }
+  }, [isLogin, navigate]); 
+
+  const jumpToPage = () => {
+    const token = localStorage.getItem("accessToken");
+    console.log("Token in jumpToPage:", token);
+    if (token) {
+      navigate("/test");
+    } else {
+      navigate("/signup");
+    }
+  };
+
   return (
     <>
       <CompNavBar />
       <div>
         <h1>무료 성격 테스트</h1>
         <h3>간단한 설명</h3>
-        <sapn>성격 유형 검사</sapn>
+        <span>성격 유형 검사</span>
         <span>성격 유형 이해</span>
         <span>팀 평가</span>
       </div>
-      <button
-        onClick={() => {
-          navigate("/login");
-        }}
-      >
-        내 성격 알아보기
-      </button>
+      <button onClick={jumpToPage}>내 성격 알아보기</button>
     </>
   );
 };
