cc3c94c47e5ff3e04fd035e142e587b1f51ea9da
mzsoba <owt4832@gmail.com>
2025-02-23 02:23:30 +0900

    feat/ 로그아웃 버튼 추가

--- src/component/CompNavBar.jsx
+++ src/component/CompNavBar.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useLoginStatus } from "../zustand/mbtiStore";
 
 const CompNavBar = () => {
   const navigate = useNavigate();
+  const { isLogin, setIsLogin } = useLoginStatus((state) => state);
+  const btnHandler = () => {
+    if (isLogin) {
+      setIsLogin(!isLogin);
+      localStorage.setItem("accessToken", "");
+    } else {
+      navigate("/signup");
+    }
+  };
   return (
     <nav>
-      <span>SPARTA MBTI</span>
-      <span
-        onClick={() => {
-          navigate("/login");
-        }}
-      >
-        로그인
-      </span>
+      <span onClick={() => navigate("/")}>SPARTA MBTI</span>
+      <span onClick={btnHandler}>{isLogin ? "로그아웃" : "로그인"}</span>
     </nav>
   );
 };
