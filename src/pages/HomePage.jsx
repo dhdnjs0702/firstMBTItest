e8981ce8d0ee1231f24b5c93a7eda5e6227116ac
mzsoba <owt4832@gmail.com>
2025-02-25 14:17:20 +0900

    feat/ tailwindcss추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import CompDescribeCard from "../component/CompDescribeCard";
 import CompNavBar from "../component/CompNavBar";
 import { useNavigate } from "react-router-dom";
 
@@ -15,13 +16,29 @@ const HomePage = () => {
     <>
       <CompNavBar />
       <div>
-        <h1>무료 성격 테스트</h1>
-        <h3>간단한 설명</h3>
-        <span>성격 유형 검사</span>
-        <span>성격 유형 이해</span>
-        <span>팀 평가</span>
+        <CompDescribeCard
+          main={"빠른 무료 검사"}
+          desc={"20문 문항으로 테스트하여 더욱 빠른 검사 가능"}
+        />
+        <CompDescribeCard
+          main={"간단한 설명"}
+          desc={"홈페이지 관련 간단한 설명이 들어갑니다"}
+        />
+        <CompDescribeCard
+          main={"성격 유형 검사"}
+          desc={"다른 사이트에서 보지 못한 신박한 문항들로 재밌게 검사해보세요"}
+        />
+        <CompDescribeCard
+          main={"팀 평가"}
+          desc={"다른 유저들의 성격 유형은 어떤지 확인해 보세요"}
+        />
       </div>
-      <button onClick={movePageHandler}>내 성격 알아보기</button>
+      <button
+        className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded"
+        onClick={movePageHandler}
+      >
+        내 성격 알아보기
+      </button>
     </>
   );
 };
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -46,22 +46,33 @@ const ProfilePage = () => {
   return (
     <div>
       <CompNavBar />
-      <h1>프로필 설정</h1>
-      <form onSubmit={btnHandler}>
-        <div>
-          <label htmlFor="nickname">닉네임(최대 8자)</label>
-          <input
-            id="nickname"
-            value={nickName}
-            onChange={handleNicknameChange}
-            maxLength={8}
-            placeholder="닉네임을 입력해주세요"
-          />
+      <div className="h-screen bg-gray-100 flex justify-center">
+        <div className="py-6 px-8 mt-20 bg-white rounded shadow-xl h-[550px] w-[500px]">
+          <h1>프로필 설정</h1>
+          <form onSubmit={btnHandler}>
+            <div>
+              <label className="block text-gray-800 font-bold">
+                닉네임 설정(최대 8자):
+              </label>
+              <input
+                id="nickname"
+                value={nickName}
+                onChange={handleNicknameChange}
+                maxLength={8}
+                placeholder="닉네임을 입력해주세요"
+                className="w-full border border-gray-300 py-3 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
+              />
+            </div>
+
+            <button
+              disabled={updateBtnMutation.isPending}
+              className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded"
+            >
+              {updateBtnMutation.isPending ? "변경 중..." : "닉네임 변경하기"}
+            </button>
+          </form>
         </div>
-        <button type="submit" disabled={updateBtnMutation.isPending}>
-          {updateBtnMutation.isPending ? "변경 중..." : "닉네임 변경하기"}
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
